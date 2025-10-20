b94636957a0ff443f6f1b9b16476d83c751f1f52
TommyTomThomas <66032463+TommyTomThomas@users.noreply.github.com>
2025-10-20 13:18:10 -0500

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx,html}","./public/index.html"],
+  theme: {
+    extend: {
+      fontFamily: { sans: ["Inter", "ui-sans-serif", "system-ui", "Segoe UI", "Roboto", "Helvetica Neue", "Arial", "Noto Sans"] },
+    },
+  },
+  plugins: [],
+};
